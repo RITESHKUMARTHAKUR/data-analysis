664d02e420a7f39f324b02912fcad378e898e34e
RITESHKUMARTHAKUR <thakurram2668@gmail.com>
2021-12-18 13:42:00 +0530

    added some codes

--- data-analysis.py
+++ data-analysis.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import numpy as np
 
+df = pd.DataFrame(pd.read_csv('train.csv'))
+df.head()
+
 df.shape
 
 df.isnull().sum()
 
 drop_col = df.isnull().sum()[df.isnull().sum()>35/100 * df.shape[0]]
 drop_col
+
 drop_col.index
 
 df.drop(drop_col.index ,axis=1 ,inplace=True)
@@ -23,7 +27,19 @@ df.isnull().sum()
 
 df.corr()
 
+df['FamilySize'] = df['SibSp'] + df['Parch']
+df.drop(['SibSp','Parch'] , axis=1 , inplace=True )
+df.corr()
 
+df['Alone'] = [ 0 if df['FamilySize'][i] >0 else 1 for i in df.index ]
+df.head()
+
+df.groupby(['Alone'])['Survived'].mean()
+
+df[['Alone','Fare']].corr()
+
+df['Sex'] = [ 0 if df['Sex'][i]=='male' else 1 for i in df.index]
+df.groupby(['Sex'])['Survived'].mean()
 
 
 df.groupby(['Embarked'])['Survived'].mean()
